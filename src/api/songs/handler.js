fcfb1c468dc0e738ee451fb6d84120ef2c28a051
Davidaprilio <david1.aprilio4@gmail.com>
2022-09-11 14:49:33 +0700

    until pass all test [Optional] Query Parameter

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -35,8 +35,11 @@ class SongsHandler {
         }
     }
 
-    async getSongsHandler() {
-        const songs = await this._service.getSongs();
+    async getSongsHandler(request) {
+        const { title, performer } = request.query;
+
+        const songs = await this._service.getSongs({ title, performer });
+
         return {
             status: 'success',
             data: {
