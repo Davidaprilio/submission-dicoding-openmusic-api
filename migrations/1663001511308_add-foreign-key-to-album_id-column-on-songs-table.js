059023788ccd523c65e2cf5e758c4e8e7a06e430
Davidaprilio <david1.aprilio4@gmail.com>
2022-09-13 00:09:59 +0700

    Cretate table playlists, playlist_songs dan Menerapkan Foreign Key ke table album_id

---
+++ migrations/1663001511308_add-foreign-key-to-album_id-column-on-songs-table.js
@@ -0,0 +1,11 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+    // memberikan constraint foreign key pada album_id terhadap kolom id dari tabel albums
+    pgm.addConstraint('songs', 'fk_songs.album_id_albums.id', 'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+    // menghapus constraint fk_songs.album_id_albums.id pada tabel songs
+    pgm.dropConstraint('songs', 'fk_songs.album_id_albums.id');
+};
